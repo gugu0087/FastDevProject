4c7fe08049fb11ab629b61073695dbb483e32928
许运昌 <xuyunchang@mzjmedia.net>
2018-08-08 11:16:13 +0800

    工具类走一波

---
+++ app/src/main/java/com/xyc/fastdevproject/utils/NetworkUtils.java
@@ -0,0 +1,23 @@
+package com.xyc.fastdevproject.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by gugu on 2018/6/29.
+ */
+
+public class NetworkUtils {
+
+    public static boolean isNetworkConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+            if (mNetworkInfo != null) {
+                return mNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
+}
